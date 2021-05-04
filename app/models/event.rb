9bf9b3c320bbc58e1f4a0fb6ff3742369f8a9263
amira <amiraabouhadid@gmail.com>
2021-05-04 20:25:21 +0200

    Associate invitations with events and users past and upcoming

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :invitation
   has_many :attendees, through: :invitation, source: :user
+
+  scope :past, -> { where('start_date_time < ?', Date.today) }
+  scope :upcoming, -> { where('start_date_time >= ?', Date.today) }
 end
